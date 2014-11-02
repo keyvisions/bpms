d7f6438b24873d2002c04d7f14d82a15448b34cd
Giancarlo Trevisan <g.trevisan@keyvisions.it>
2014-11-02 08:07:26 +0000

    Better drag and drop

--- scripts/bpmn.js
+++ scripts/bpmn.js
@@ -1,18 +1,22 @@
-var paper = null;
+var diagram = null;
 var selectedPath = null;
 var bpmn = {
-    "event": { name: "Event", set: ["M-25,0a25,25 0 1,0 50,0a25,25 0 1,0 -50,0"] },
-    "action": { name: "Action", set: ["M-75,-50l150,0l0,100l-150,0Z"] },
-    "gateway": { name: "Gateway", set: ["M0,-25l25,25l-25,25l-25,-25Z"] },
-    "lane": { name: "Lane", set: ["M0,-75l400,0l0,150l-400,0l0,-150m30,0l0,150"] },
-    "pool": { name: "Pool", set: ["M0,-75l400,0l0,150l-400,0l0,-150m24,0l0,150"] },
-    "text": { name: "Text", set: ["M20,-50l-20,0l0,100l20,0"] }
+    event: { title: "Event", set: [{ path: "M-25,0a25,25 0 1,0 50,0a25,25 0 1,0 -50,0" }] },
+    action: { title: "Action", set: [{ path: "M-75,-50l150,0l0,100l-150,0Z" }] },
+    gateway: { title: "Gateway", set: [{ path: "M0,-25l25,25l-25,25l-25,-25Z" }] },
+    lane: { title: "Lane", set: [{ path: "M0,-75l400,0l0,150l-400,0l0,-150m30,0l0,150" }, { text: "Lane", transform: "t10,0r90" }] },
+    pool: { title: "Pool", set: [{ path: "M0,-75l400,0l0,150l-400,0l0,-150m24,0l0,150" }, { path: "M0,-25l25,25l-25,25l-25,-25Z" }] },
+    textAnnotation: { title: "Text Annotation", set: [{ path: "M20,-50l-20,0l0,100l20,0" }, { text: "Annotation" }] },
+    flow: { title: "Flow", set: [{ path: "M0,0l200,0l0,-5l10,5l-10,5l0,-5", style: { fill: "#000000" } }] },
+    mail: { title: "Mail", set: [{ path: "M20,-15l-20,15l-20,-15l40,0l0,30l-40,0l0,-30" }] }
 };
 
 function init() {
-    paper = Raphael(0, 0, "100%", "100%");
+    diagram = Raphael(0, 0, "100%", "100%");
     
-    paper.canvas.addEventListener("contextmenu", function(e) {
+    createBPMNMenu();
+
+    diagram.canvas.addEventListener("contextmenu", function(e) {
         e.preventDefault();
         var menu = document.getElementById("bpmnmenu");
         menu.style.display = "block";
@@ -20,8 +24,9 @@ function init() {
         menu.style.top = e.clientY + "px";
         return false;
     }, false);
-    
-    createBPMNMenu();
+    document.body.addEventListener("click", function (e) {
+        document.getElementById("bpmnmenu").style.display = "none";
+    });
 }
 
 function createBPMNMenu() {
@@ -29,41 +34,45 @@ function createBPMNMenu() {
     for (var symbol in bpmn) {
         var li = document.createElement("li");
         li.setAttribute("data-type", symbol);
-        li.appendChild(document.createTextNode(bpmn[symbol].name));
-        li.addEventListener("click", function(e) {
-            createSymbol(e.target.getAttribute("data-type"));
-        });
+        li.appendChild(document.createTextNode(bpmn[symbol].title));
         ul.appendChild(li);
     }
+    ul.addEventListener("click", function(e) {
+        createSymbol(e.target.getAttribute("data-type"));
+    });
 }
 
 function createSymbol(symbol) {
-    paper.setStart();
-    for (var path in bpmn[symbol].set) {
-        paper.path(bpmn[symbol].set[path]).attr({ 'fill':'#FFFFFF' });
+    if (symbol) {
+        diagram.setStart();
+        for (var i in bpmn[symbol].set) {
+            var subset = bpmn[symbol].set[i];
+            if (subset.path)
+                diagram.path(subset.path).attr(subset.style || { fill: '#FFFFFF' }).transform(subset.transform || "");
+            else if (subset.text)
+                diagram.text(subset.x || 0, subset.y || 0, subset.text).transform(subset.transform || "");
+        }
+        var set = diagram.setFinish();
+        set.forEach(function(subset) { subset.set = set; });
+        set.drag(move, start, up);
     }
-    var symbolSet = paper.setFinish();
-    symbolSet.drag(move, start, up);
-    return symbolSet;
 }
 
-var start = function() {
-        this.lastdx ? this.odx += this.lastdx : this.odx = 0;
-        this.lastdy ? this.ody += this.lastdy : this.ody = 0;
-        this.animate({
-            "fill-opacity": 0.2
-        }, 500);
-    },
-    move = function(dx, dy) {
-        this.transform("T" + (dx + this.odx) + "," + (dy + this.ody));
-        this.lastdx = dx;
-        this.lastdy = dy;
-    },
-    up = function() {
-        this.animate({
-            "fill-opacity": 1
-        }, 500);
-    };
+function start() {
+    var set = this.set;
+    set.dx = 0, set.dy = 0;
+    set.animate({ "fill-opacity": 0.2 }, 500);
+}
+function move(dx, dy) {
+    var set = this.set;
+    set.transform("...T"+-set.dx+","+-set.dy);
+    set.transform("...T"+dx+","+dy);
+    set.dx = dx, set.dy = dy;
+}
+function up() {
+    var set = this.set;
+set.animate({ "fill-opacity": 1 }, 500);
+}
 
 function setPath(newPath) {
     if (selectedPath && newPath) selectedPath.attr("path", newPath);
